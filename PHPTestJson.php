79448ed7241f5e7b5db439587c8644b4a48b7a99
pradityamsook <praditcry@gmail.com>
2017-03-17 22:49:25 +0700

    Delete PHPTestJson.php

--- PHPTestJson.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $json_string = file_get_contents("http://api.wunderground.com/api/Your_Key/geolookup/conditions/q/IA/Cedar_Rapids.json");
-  $parsed_json = json_decode($json_string);
-  $location = $parsed_json->{'location'}->{'city'};
-  $temp_f = $parsed_json->{'current_observation'}->{'temp_f'};
-  echo "Current temperature in ${location} is: ${temp_f}\n";
-?>
